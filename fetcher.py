b4e15b9c4c5bdadbf544343a313a4c48c09b7954
Ludovic Delaveau <ldelaveau@gmail.com>
2010-12-19 21:44:03 +0100

    Added comments everywhere, and made some minor changes

--- fetcher.py
+++ fetcher.py
@@ -9,6 +9,7 @@ from urllib2 import URLError, HTTPError
 import urlparse
 import robotparser
 import re
+import time
 
 # modules from the project
 import logger
@@ -22,19 +23,20 @@ class Crawler:
 
 
     def __init__(self, base_url):
-        self.__url_to_visit.append(base_url)
+        """Constructor"""
+        self.__url_to_visit.append(base_url) # start url
         self.__logger_instance = logger.Logger('crawler', """This is a dummy\
  crawler, written by Nicolas Bontoux and Ludovic Delaveau.\nAnd here is a log\
- entry. """)
+ entry. """) # initializes the logger used to keep track of the event
 
         split_url = urlparse.urlparse(base_url)
-
         if (split_url.scheme != 'http'):
             self.__logger_instance('Exiting', 'sorry this crawler supports'
                                    + 'only the http protocol\n'
                                    + 'Exiting now ...')
             sys.exit(1)
 
+        # __rp is the nice robot taking care of the webmaster directives
         self.__rp = robotparser.RobotFileParser()
         self.__current_domain = split_url.scheme + '://'\
                                         + split_url.netloc + '/'
@@ -45,7 +47,8 @@ class Crawler:
         """Fetches the url given as input and returns the page. Returns an empty
         string when an exception occured.
         """
-        if (not self.__rp.can_fetch("*", url)):
+        # checks the robots.txt directives
+        if (not self.__rp.can_fetch("*", url)): 
             self.__logger_instance.log_event('robot non authorized', [url])
             return ''
 
@@ -56,7 +59,8 @@ class Crawler:
             req = urllib2.Request(url, data, self.__headers)
 
             response = urllib2.urlopen(req)
-            html = response.read()
+            if (response.info().gettype() == 'text/html'):
+                html = response.read()
         except HTTPError, he:
             self.__logger_instance.log_event('HTTPError', [str(he.code) + ' ',
                                                       he.read()])
@@ -68,8 +72,8 @@ class Crawler:
     def get_anchors(self, html_page, base_url):
         """Searches the anchors in a html page"""
         anchor_re = re.compile(r'<a\s*href=[\'|"](.*?)[\'|"].*>')
-
         links = anchor_re.findall(html_page)
+
         if (links.__len__() > 0):
             self.__logger_instance.log_event('New links found', links)
         
@@ -80,6 +84,7 @@ class Crawler:
                 link = urlparse.urljoin(base_url, link)
             elif (not link.startswith('http')):
                 link = urlparse.urljoin(base_url, link)
+
             if (link not in self.__url_to_visit and link not in
                 self.__url_visited):
                 self.__url_to_visit.append(link)
@@ -89,8 +94,8 @@ class Crawler:
         """Looks for keywords in the page"""
         keyword_re = re.compile(r'<meta\sname=[\'|"]keywords[\'|"]\s' 
                    + r'content=[\'|"](.*?)[\'|"].*>')
-
         keywords = keyword_re.findall(html_page)
+
         if (keywords.__len__() > 0):
             self.__logger_instance.log_event('New keywords found'
                                              + ' for the current page',
@@ -100,13 +105,12 @@ class Crawler:
     def set_domain(self, domain):
         """Sets the domain of the current request in order to use the robots
         directives."""
-        self.domain  = domain
         robots_url = urlparse.urljoin(domain, 'robots.txt')
         try:
             self.__logger_instance.log_event('Robots on %s' % domain,
                                              urllib2.urlopen(robots_url))
             self.__rp.set_url(urlparse.urljoin(domain, 'robots.txt'))
-            self.__rp.read()
+            self.__rp.read() # parses the content of the directives
         except HTTPError, e:
             self.__logger_instance.log_short_message('Could not get robots on '
                                                      + domain + ' ' + str(e))
@@ -118,6 +122,7 @@ class Crawler:
     def handler(self):
         i = 0
         while (i < 100 and self.__url_to_visit.__len__() > 0):
+            i = i + 1
             current_url = self.__url_to_visit.pop(0)
             split_url = urlparse.urlparse(current_url)
             new_domain = split_url.scheme + '://' + split_url.netloc + '/'
@@ -127,10 +132,12 @@ class Crawler:
                 self.set_domain(self.__current_domain)
 
             page = self.fetch_page(current_url)
+            if (page = ''): # page is empty, no need to do anything with it
+               continue
+
             self.__url_visited.append(current_url)
             self.get_anchors(page, current_url)
             self.get_keywords(page, current_url)
-            i = i + 1
 
 
 if __name__ == '__main__':

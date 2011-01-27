50b8d8936d0f0f1fe63219f2fce9170cddf8450d
Ludovic Delaveau <ldelaveau@gmail.com>
2011-01-27 23:42:51 +0100

    Wrote the syncronized dictionary, needs some testing now. But first let's have some sleep.

--- data_utils.py
+++ data_utils.py
@@ -3,10 +3,11 @@
 
 
 from datetime import datetime
-from Queue import Queue, Empty
+from Queue import Queue, Empty, Full
 import sqlite3
 import threading
 from threading import local
+from time import time as _time
 import os
 
 
@@ -56,7 +57,249 @@ class SortedQueue(Queue):
 
 
 
-class data_persistance:
+class SynchronizedDict:
+    """Class implementing a synchronized dictionary, allowing concurential
+    access to the elements of the dict. It is a special dict as values are
+    stored as a list, so that you can add elements to an existing key if
+    needed, thanks to the add_item_to_key method.
+    To retrieve an element from the dict you have two choices, either calling
+    get which will return an arbitrary mapping from the dict, or calling the
+    get_with_key method which will return only the list of values.
+    It does not provide any iterator either on the keys or the values.
+    This class is largely inspired by the Queue class from the standard
+    library."""
+    def __init__(self, maxsize=0):
+        try:
+            import threading
+        except ImportError:
+            import dummy_threading as threading
+        self.maxsize = maxsize
+        self._init(maxsize)
+        self.mutex = threading.Lock()
+        self.not_full = threading.Condition(self.mutex)
+        self.not_empty = threading.Condition(self.mutex)
+        self.all_tasks_done = thrading.Condition(self.mutex)
+        self.unfinished_tasks = 0
+
+
+    def task_done(self):
+        """Indicate that a formerly enqueued task is complete.
+
+        Used by Queue consumer threads.  For each get() used to fetch a task,
+        a subsequent call to task_done() tells the queue that the processing
+        on the task is complete.
+
+        If a join() is currently blocking, it will resume when all items
+        have been processed (meaning that a task_done() call was received
+        for every item that had been put() into the queue).
+
+        Raises a ValueError if called more times than there were items
+        placed in the queue.
+        """
+        self.all_tasks_done.acquire()
+        try:
+            unfinished = self.unfinished_tasks - 1
+            if unfinished  <= 0:
+                if unfinished < 0:
+                    raise ValueError('task_done() called too many times')
+                self.all_tasks_done.notify_all()
+            self.unfinished_tasks = unfinished
+        finally:
+            self.all_tasks_done.release()
+
+
+    def join(self):
+        """Blocks until all items in the Queue have been gotten and processed.
+
+        The count of unfinished tasks goes up whenever an item is added to the
+        queue. The count goes down whenever a consumer thread calls task_done()
+        to indicate the item was retrieved and all work on it is complete.
+
+        When the count of unfinished tasks drops to zero, join() unblocks.
+        """
+        self.all_tasks_done.acquire()
+        try:
+            while self.unfinished_tasks:
+                self.all_tasks_done.wait()
+        finally:
+            self.all_tasks_done.release()
+
+
+    def qsize(self):
+        """Return the approximate size of the queue (not reliable!)."""
+        self.mutex.acquire()
+        n = self._qsize()
+        self.mutex.release()
+        return n
+
+
+    def empty(self):
+        """Return True if the queue is empty, False otherwise (not reliable!)."""
+        self.mutex.acquire()
+        n = not self._qsize()
+        self.mutex.release()
+        return n
+
+
+    def full(self):
+        """Return True if the queue is full, False otherwise (not reliable!)."""
+        self.mutex.acquire()
+        n = 0 < self.maxsize == self._qsize()
+        self.mutex.release()
+        return n
+
+
+    def put(self, key, item, block=True, timeout=None):
+        """Put in the dict the mapping between an item and a dict.
+
+        If optional args 'block' is true and 'timeout' is None (the default),
+        block if necessary until a free slot is available. If 'timeout' is
+        a positive number, it blocks at most 'timeout' seconds and raises
+        the Full exception if no free slot was available within that time.
+        Otherwise ('block' is false), put an item on the queue if a free slot
+        is immediately available, else raise the Full exception ('timeout'
+        is ignored in that case).
+        """
+        self.not_full.acquire()
+        try:
+            if self.maxsize > 0:
+                if not block:
+                    if self._qsize() == self.maxsize:
+                        raise Full
+                elif timeout is None:
+                    while self._qsize() == self.maxsize:
+                        self.not_full.wait()
+                elif timeout < 0:
+                    raise ValueError("'timeout' must be a positive number")
+                else:
+                    endtime = _time() + timeout
+                    while self._qsize() == self.maxsize:
+                        remaining = endtime - _time()
+                        if remaining <= 0.0:
+                            raise Full
+                        self.not_full.wait(remaining)
+            self._put(key, item)
+            self.unfinished_tasks += 1
+            self.not_empty.notify()
+        finally:
+            self.not_full.release()
+
+
+    def put_nowait(self, key, item):
+        """Put the mapping between the key and the item in the dict without
+        blocking.
+
+        Only enqueue the item if a free slot is immediately available.
+        Otherwise raise the Full exception.
+        """
+        return self.put(key, item, False)
+
+    
+    def add_item_to_key(self, key, item):
+        """Method to add a value to an existing key. Use put with the name of
+        the key if you want to overwrite the current value associated with the
+        key if it exists.
+        Adding an item to an existing key shouldn't block in any way, that's
+        why there isn't a block or timeout argument"""
+        self.not_full.acquire()
+        try:
+            self._add_item_to_key(key, item) 
+        finally:
+            self.not_full.release()
+
+
+    def get_with_key(self, key):
+        """Removes and returns the item corresponding to the given key from the
+        dict. It raises Empty if no key was found in the dict, KeyError if the
+        specific key is not in the dictionary.
+
+        This call is not blocking, as you want a specific entry in the dict: it
+        is there and is returned, or it's not there, and a KeyError exception
+        is raised.
+        """
+        self.not_empty.acquire()
+        try:
+            if not self._qsize():
+                raise Empty
+            else:
+                item = self._get(key)
+                self.not_full.notify()
+                return item
+        finally:
+            self.not_empty.release()
+
+
+    def get(self, block=True, timeout=None):
+        """Removes and returns one arbitrary mapping. Contrary to the 
+        get_with_key method, this call can be blocking.
+
+        If optional args 'block' is true and 'timeout' is None (the default),
+        block if necessary until an item is available. If 'timeout' is
+        a positive number, it blocks at most 'timeout' seconds and raises
+        the Empty exception if no item was available within that time.
+        Otherwise ('block' is false), return an item if one is immediately
+        available, else raise the Empty exception ('timeout' is ignored
+        in that case)."""
+        self.not_empty.acquire()
+        try:
+            if not block:
+                if not self._qsize():
+                    raise Empty
+            elif timeout is None:
+                while not self._qsize():
+                    self.not_empty.wait()
+            elif timeout < 0:
+                raise ValueError("'timeout' must be a positive number")
+            else:
+                endtime = _time() + timeout
+                while not self._qsize():
+                    remaining = endtime - _time()
+                    if remaining <= 0.0:
+                        raise Empty
+                    self.not_empty.wait(remaining)
+            item = self._get()
+            self.not_full.notify()
+            return item
+        finally:
+            self.not_empty.release()
+
+
+    def get_nowait(self):
+        """Remove and return an arbitrary mapping from the dict without
+        blocking.
+
+        Only get a mapping if one is immediately available. Otherwise
+        raise the Empty exception.
+        """
+        return self.get(False)
+
+
+    def _init(self, maxsize=0):
+        self.queue = {}
+
+    def _qsize(self):
+        return len(self.queue)
+
+
+    def _put(self, key, item):
+        self.queue[key] = list(item)
+
+    
+    # adds an item to an existing place
+    def _add_item_to_key(key, item):
+        self.queue[key] = self.queue[key].append(item)
+
+
+    # returns the value of the key
+    def _get_with_key(self, key):
+        return self.queue.pop(key)
+
+    
+    def _get(self, key):
+        return self.popitem()
+
+
+class DataPersistance:
     """Class to write down the datas collected"""
     def __init__(self):
         self._init_folders()
@@ -71,7 +314,7 @@ class data_persistance:
 
 
     def _init_folders(self):
-        """Creates the folder for the current day and then the """
+        """Creates the folder for the current day and then the actual time"""
         try:
             if (self.prefix):
                 return
@@ -104,17 +347,30 @@ class data_persistance:
 
     def dump_pool_to_html(pool):
         keywords, anchors_list, _ , text_content = pool
+        # get all the domains for the urls, and make as many folders as there
+        # are domains. Create a global index which will points to each of the
+        # domains, which will then have themselves a summary page containing
+        # all the urls of the domain
+        # so it implies constituing a dict of domains as keys with a list of
+        # the urls from the domain as the values, and write at the end of the
+        # processing all the domains files.
+        # And same thing for the global summary, pointing to all the domains
 
 
 
 class FileWriter(threading.Thread):
-    def __init__(self, group=None, target=None, name=None, *args, **kwargs):
+    """Thread writing to the disk the dump of one page, i.e. the url, the links
+    and the corresponding anchors, the keywords, the various scores and finally
+    the content of the page cleant of the html tags."""
+    def __init__(self, path, group=None, target=None, name=None, *args, **kwargs):
         threading.Thread.__init__(self, name=name, args=args, kwargs=kwargs)
+        self._path = path
         self._my_data = local()
 
 
     def create_one_page(self, page_name, anchors, keywords, text_content):
         page = self._prepare_header(page_name)
+        page = ''.join([page, self._prepare_footer])
 
 
     def _prepare_header(self, title):
